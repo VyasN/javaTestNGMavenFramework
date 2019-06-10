2d3330053546d6abd003a8d52b9d516a8c6c25de
Nartan <nartanvyasqa@gmail.com>
2019-06-10 15:38:12 -0400

    Add new Test Method in Test File with comments

--- src/test/java/testApp/DemoGmail.java
+++ src/test/java/testApp/DemoGmail.java
@@ -5,15 +5,24 @@ import org.testng.annotations.Test;
 import utils.BaseTest;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.AfterMethod;
 
 public class DemoGmail extends BaseTest {
 
-// before class and after class will tear up and down browser
-// which is already implemented in BaseTest.class
+	// before class and after class will tear up and down browser
+	// which is already implemented in BaseTest.class
 	@Test
 	public void checkEmails() {
 		// Check you have new email or not
+		// Wright test which ever you want
+		// As per the best practises to write all methods
+		// Inherit all elements from element and method classes
+		// Which chould be separatly created under page package as separate class
+	}
+
+	@Test
+	public void deleteEmail() {
 		// Delete email which is older than 30 days
 		// Wright test which ever you want
 		// As per the best practises to write all methods
